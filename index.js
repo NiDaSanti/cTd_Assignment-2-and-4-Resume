85e96c540022961c0e0822a23bd79351640a60d0
Alton Michaux <alton.michaux@yahoo.com>
2020-11-28 18:15:30 -0500

    changed img from square to circle as well as added some hover effects

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 
 let musicQ = () => {
     let name = prompt("Hey, what's your name?");
-
     let invite = prompt(`Nice name ${name}, you like music?`);
     if (invite==="yes") {
-        alert(`I knew you were cool ${name}.`);
+        alert(`I knew you were cool ${name}. You can check out my playlist as you browse my front page.`);
     }
         else {
             alert(`Hey, different strokes for different folks. I respect that ${name}`);
