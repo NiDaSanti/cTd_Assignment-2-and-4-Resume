bf37d91c53abba21d58daaeb6e3efc537dc0085e
Alton <alton.michaux@yahoo.com>
2020-11-28 12:01:35 -0500

    edited javascript to aske two questions and give a conditional response

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let name = prompt("Hey, what's your name?")
+let musicQ = () => {
+    let name = prompt("Hey, what's your name?");
 
-alert(`Thanks for visiting my page ${name}!`);
+    let invite = prompt(`Nice name ${name}, you like music?`);
+    if (invite==="yes") {
+        alert(`I knew you were cool ${name}`);
+    }
+        else {
+            alert(`Hey, different strokes for different folks. I respect that ${name}`);
+        }
+};
+
+musicQ();
